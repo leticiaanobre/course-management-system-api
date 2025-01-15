3402a52bef5a3fbe535a26e18c10139385199950
Leticia <leticiavidal2910@gmail.com>
2025-01-15 10:27:35 -0300

    refat: consoles and response user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,10 +3,30 @@ const moment = require('moment-timezone');
 
 const createUser = async (req, res) => {
   try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({error: "Name, email and password are required."});
+    }
+
+    const existingUser = await User.findOne({ where: {email}});
+    if(existingUser) {
+      return res.status(400).json({error: "Email already exists."})
+    }
+
+    const user = await User.create({name, email, password});
+    res.status(201).json({
+      message: 'User created successfully',
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: moment(user.created_at).tz('UTC').format()
+      }
+    });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    console.error(error);
+    res.status(500).json({error:"Internal server error."})
   }
 };
 
@@ -16,11 +36,20 @@ const getUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
+
+    //fuso horário do cliente
     const clientTimezone = req.query.timezone || 'UTC';
     const adjustedCreatedAt = moment(user.created_at).tz(clientTimezone).format();
-    res.json({ ...user.toJSON(), created_at: adjustedCreatedAt });
+    
+    res.json({ 
+      id: user.id,
+      name: user.email,
+      email: user.email,
+      created_at: adjustedCreatedAt,
+      message: "User data retrived successively."
+     });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: "Internal server error." });
   }
 };
 
