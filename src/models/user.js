4eeae5baa7bff147b5f6b80d84de9f954648f0dc
Leticia <leticiavidal2910@gmail.com>
2025-01-14 22:54:37 -0300

    feat: added user model

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const { Sequelize } = require('sequelize');
+const config = require('../config/database.js');
+
+const environment = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[environment]);
+
+class User extends Model {}
+
+User.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    timestamps: false,
+  }
+);
+
+User.beforeCreate(async (user) => {
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+});
+
+module.exports = User;
